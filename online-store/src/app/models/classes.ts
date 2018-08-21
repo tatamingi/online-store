7b730c8a1e433c7cff76e6151f2bad74bdf197bd
TShapovalova <tatamingi@gmail.com>
2018-08-21 10:42:07 +0300

    take hold footer

--- online-store/src/app/models/classes.ts
+++ online-store/src/app/models/classes.ts
@@ -4,3 +4,13 @@ export class Category {
     public items: string[]
   ) {}
 }
+
+export class Product {
+  constructor(
+    public id: string,
+    public title: string,
+    public price: number,
+    public category: string,
+    public imageUrls: string[],
+  ) {}
+}
